30efb723e742a1d1531b1a906552388070719bf1
hyeuhieu123 <147390311+hyeuhieu123@users.noreply.github.com>
2024-11-24 21:04:03 +0700

    Update b1_server.java

--- b1_server.java
+++ b1_server.java
@@ -15,21 +15,21 @@ public class b1_server {
         DatagramSocket server = new DatagramSocket(9999);
         System.out.println("Server đang chạy, chờ kết nối...");
 
-        // Nhận dữ liệu từ client
+       
         byte[] nhann = new byte[1000];
         DatagramPacket nhanson = new DatagramPacket(nhann, nhann.length);
         server.receive(nhanson);
         int n = Integer.parseInt(new String(nhanson.getData(), 0, nhanson.getLength()));
 
-        // Tìm các số nguyên tố chia hết cho 5 nhỏ hơn n
+      
         StringBuilder result = new StringBuilder();
-        for (int i = 5; i < n; i += 5) { // chỉ kiểm tra số chia hết cho 5
+        for (int i = 5; i < n; i += 5) {
             if (isPrime(i)) {
                 result.append(i).append(" ");
             }
         }
 
-        // Gửi kết quả về client
+        
         byte[] guisodulieu = result.toString().getBytes();
         DatagramPacket guisodem = new DatagramPacket(guisodulieu, guisodulieu.length, nhanson.getAddress(), nhanson.getPort());
         server.send(guisodem);
@@ -37,7 +37,7 @@ public class b1_server {
         server.close();
     }
 
-    // Kiểm tra số nguyên tố
+   
     public static boolean isPrime(int num) {
         if (num < 2) return false;
         for (int i = 2; i <= Math.sqrt(num); i++) {
